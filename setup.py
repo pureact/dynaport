9b1ce7d0fdf7e6f0859ad936c04c3c37ca0cd559
NAEK2K <naek2k@outlook.com>
2021-01-24 17:46:29 -0500

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="dynaport",
-    version="0.0.5",
+    version="0.0.6",
     author="naek",
     author_email="naek2k@outlook.com",
     description="dynamic module importer",
