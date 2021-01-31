a41c6b8fb66a22f04009f3e42bd493cb4af97ccd
NAEK2K <naek2k@outlook.com>
2021-01-31 00:58:26 -0500

    un-refactoring

---
+++ dynaport/__init__.py
@@ -0,0 +1,1 @@
+from dynaport import Dynaport
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="dynaport",
-    version="0.0.10",
+    version="0.0.12",
     author="naek",
     author_email="naek2k@outlook.com",
     description="dynamic module importer",
