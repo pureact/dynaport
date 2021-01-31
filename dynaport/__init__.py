9ae7bf30f1ce5275445bf8e66e91cc4c9ff49973
NAEK2K <naek2k@outlook.com>
2021-01-31 01:04:25 -0500

    un-fixing my broken fix

--- dynaport/__init__.py
+++
@@ -1,1 +0,0 @@
-from dynaport import Dynaport
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="dynaport",
-    version="0.0.13",
+    version="0.0.15",
     author="naek",
     author_email="naek2k@outlook.com",
     description="dynamic module importer",
