bcd2f15ac149939b9255c868bb63408118440e37
NAEK2K <naek2k@outlook.com>
2021-01-31 00:44:01 -0500

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="dynaport",
-    version="0.0.9",
+    version="0.0.10",
     author="naek",
     author_email="naek2k@outlook.com",
     description="dynamic module importer",
